9df4752b3cf1ce0459067b0a91a0e933fac24872
Clinton <clintonchakma.24csedu.064@gmail.com>
2023-09-12 15:58:14 +0600

    Shopping Cart entities Added

---
+++ src/Services/Basket/Basket.API/Enitity/ShoppingCartItem.cs
@@ -0,0 +1,11 @@
+﻿namespace Basket.API.Enitity
+{
+    public class ShoppingCartItem
+    {
+        public int Quantity { set; get; }
+        public decimal Price { set; get; }
+        public string ProductName { set; get; }
+        public string ProductId { set; get; }
+        public string Color { set; get; }
+    }
+}
